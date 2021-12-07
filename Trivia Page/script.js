23b1e634cdebf8e96fce9aa3bea6fcb8b5432054
Labrot <sarah.lientz@gmail.com config --global user.email>
2021-12-07 14:04:55 -0500

    Started on js for trivia page and fetched from 1 of 2 apis to be used on this page

---
+++ Trivia Page/script.js
@@ -0,0 +1,22 @@
+fetch("https://anime-facts-rest-api.herokuapp.com/api/v1/")
+    .then(response => response.json())
+    .then(info => { 
+        console.log(info)
+        for (i=0; i<info.data.length; i++){
+            var newH = document.createElement("h2");
+            newH.textContent = info.data[i].anime_name;
+            document.querySelector("body").appendChild(newH)
+        }
+    })
+
+// var obj = {
+//     dogN: "Fido",
+//     test: {
+//         name: "Lewis",
+//         moreTest: {
+//             superName: "Batman"
+//         }
+//     }
+// }
+
+// console.log(obj.test.moreTest.superName)
