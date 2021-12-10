2cd4e29db8f89a8126e9001a5480632cac46fe9f
Labrot <sarah.lientz@gmail.com config --global user.email>
2021-12-09 21:40:22 -0500

    Made changes to homepage with new gif and edits to trivia page

--- Trivia Page/assets/script.js
+++ Trivia Page/assets/script.js
@@ -1,3 +1,56 @@
+// Script edits Thur Dec 9
+
+$(document).ready(function() {
+    console.log("ready!");
+
+
+
+
+// //Get select object
+
+// //Get select object
+// var AnimeSelected = document.getElementById("Selected");
+
+// //Set selected
+// showAnime(AnimeSelected, "13");
+
+// function showAnime(selected, valueToSet) {
+//     for (var i = 0; i < AnimeSelected.options.length; i++) {
+//         if (AnimeSelected.options[i].text== valueToSet) {
+//             AnimeSelected.options[i].selected = true;
+//             return;
+//         }
+//     }
+// }
+
+
+
+
+// onclick when user selects anime character
+$("#Selected").on("click", function(event) {
+    event.preventDefault();
+    EventTarget.addEventListener()
+    var AnimeSelected = $("#Selected").val() //save the anime character selected
+    var allAnime= []; //Array to hold all selected anime characters
+
+    allAnime= JSON.parse(localStorage.getItem("allAnime")) || []; //Get Anime Characters
+    allAnime.push(AnimeSelected); //pushes new anime selected to array
+    localStorage.setItem("allAnime", JSON.stringify(allAnime)); //save anime selected to local storage
+
+
+    showAnime(AnimeSelected);
+}); //End of anime selection onclick storage
+
+
+
+
+
+
+
+
+
+
+// DAY 1 JAVASCRIPT TO SEE IF API WORKED
 fetch("https://anime-facts-rest-api.herokuapp.com/api/v1/")
     .then(response => response.json())
     .then(info => { 
