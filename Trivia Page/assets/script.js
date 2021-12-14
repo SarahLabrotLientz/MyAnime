7523133a2fb8bbd9cd6eb922299da8e5dad06b25
Labrot <sarah.lientz@gmail.com config --global user.email>
2021-12-14 14:15:32 -0500

    updated styling on trivia page

--- Trivia Page/assets/script.js
+++ Trivia Page/assets/script.js
@@ -6,26 +6,33 @@ var selectElement = document.querySelector(".Selected");
 selectElement.addEventListener('change', (event) => {
 
     var result = document.querySelector(".result");
-    result.textContent = event.target.value;
+    // result.textContent = event.target.value;
     var chosen = event.target.value;
     loadSelectedAnime(chosen);
     var allchosen = []; //array to hold all selected anime characters
 
-    allchosen = JSON.parse(localStorage.getItem("allchosen")) || []; //get previously chosen
-    allchosen.push(chosen); //pushes new choices selected in array
-    localStorage.setItem("allchosen", JSON.stringify(allchosen)); //saves chosen anime to local storage
+    // allchosen = JSON.parse(localStorage.getItem("allchosen")) || []; //get previously chosen
+    // allchosen.push(chosen); //pushes new choices selected in array
+    // localStorage.setItem("allchosen", JSON.stringify(allchosen)); //saves chosen anime to local storage
 
     
 });
 
 
-//Saturday Work 
+//empties out previous data so that it only shows selected content
+
+$(".result").empty();
 
 
 
   //to open API for images and facts
 
 function loadSelectedAnime(name) {
+
+    //empties out previous data so that it only shows selected content
+
+$(".result").empty();
+
     var animeURL = "https://anime-facts-rest-api.herokuapp.com/api/v1/" + name;
     fetch(animeURL)
         .then(response => response.json())
@@ -34,26 +41,21 @@ function loadSelectedAnime(name) {
             var newimage = document.createElement("img");
             newimage.setAttribute("src", info.img);
 
-            document.querySelector("body").appendChild(newimage);
+//creating attribute "class" to style the above
+            // newimage.setAttribute("class")
+            document.querySelector(".result").appendChild(newimage);
 
             for (var i=0; i<info.data.length; i++){
                 var newCard = document.createElement("h2");
                 newCard.textContent = info.data[i].fact;
-                document.querySelector("body").appendChild(newCard);
+                document.querySelector(".result").appendChild(newCard);
                 console.log(info.data[i].fact);
             }
 
-            //Trying to style results
-                // $(".results").append(
-                //     "<div class='columns is-centered'>"
-                //     +  "<div class='column'>"
-                //     +  "<div class='card-image'>" + newimage +"</div>"
-                //     +  "<div class='card-text'>" + "<img src='" + newimage+ "'>" +"</div>"
-                     
-                //     + "</div>" 
-                // )
+            
         })
 }
+
 
 
 
