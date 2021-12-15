776c729722eba17761a60bb41bfa447664ae6ad9
mkang987 <mkang987@gmail.com>
2021-12-14 20:46:04 -0500

    Finished styling and got anime cards to work properly

--- News Page/script.js
+++ News Page/script.js
@@ -2,15 +2,20 @@ var seasonListEl = document.querySelector("#seasonalList");
 var searchAnime = document.querySelector("#searchForm");
 var cardClass = document.getElementsByClassName("card");
 
+
+//Function to check which year and season is chosen.
 function checkSearchForm(event) {
     event.preventDefault();
     var seasonInput = document.querySelector("#seasons").value;
     var yearInput = document.querySelector("#years").value;
     console.log(seasonInput);
     console.log(yearInput);
+    $("#seasonalList").empty();
     loadSeasonAnime(yearInput,seasonInput);
 }
 
+
+//Function to fetch the anime list from the Jikan API and get it to create the list.
 function loadSeasonAnime(year,season) {
     const seasonURL = "https://api.jikan.moe/v3/season/"+ year +"/" + season;
     fetch(seasonURL)
@@ -23,8 +28,7 @@ function loadSeasonAnime(year,season) {
       })
     .then(function(animeList) {
         console.log(animeList);
-        //seasonListEl.parentNode.removeChild(seasonListEl);
-        for(var x = 0; x < 10; x++) {
+        for(var x = 0; x < 21; x++) {
             createList2(animeList.anime[x]);
         }
         
@@ -32,15 +36,12 @@ function loadSeasonAnime(year,season) {
     })
     .catch(err=>console.warn(err.message))
 }
-   
-searchAnime.addEventListener('submit',checkSearchForm);
 
-
+//Function to create card for each fetched anime from API.
 function createList2(anime) {
-    var animeList = document.createElement('li');
 
     var newCard = document.createElement("div");
-    newCard.setAttribute("class", "card is-one-third");
+    newCard.setAttribute("class", "card column is-one-quarter");
 
     var newImg = document.createElement("img");
     newImg.setAttribute("src", anime.image_url);
@@ -49,8 +50,10 @@ function createList2(anime) {
     newTitle.setAttribute("class", "card-content");
     newTitle.textContent = anime.title;
 
-    seasonListEl.appendChild(animeList);
-    animeList.appendChild(newCard);
+    seasonListEl.appendChild(newCard);
     newCard.appendChild(newImg);
     newCard.appendChild(newTitle);
 }
+
+//Event listener for search button
+searchAnime.addEventListener('submit',checkSearchForm);
